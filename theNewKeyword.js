b3918a9d290e98d5359f19602fca2f280e297557
Hao J <582307236@qq.com>
2018-02-12 23:52:28 +0800

    Create theNewKeyword.js

---
+++ theNewKeyword.js
@@ -0,0 +1,6 @@
+var a = new Object();
+
+//1. A new empty object is created.
+//2. The object is linked to the protoptype of Object function.
+//3. The 'this' context is bond to the new object itself.
+//4. 'This' gets returned.
